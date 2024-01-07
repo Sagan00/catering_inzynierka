f93b12f5ce4174e7600e76505b34394396eb52f4
Sagan00 <wsaganowski14@gmail.com>
2024-01-07 11:43:34 +0100

    Dodawanie oraz modyfikacja adresów

---
+++ Projekt_Inżynierski/client/src/components/Account/AddressChange.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import { Form, Row, Col, Button, Toast, ToastContainer } from "react-bootstrap";
+import axios from "axios";
+
+const AddressChange = ({ onAddressAdded }) => {
+  const [show, setShow] = useState(false);
+  const [newAddress, setNewAddress] = useState({
+    streetName: "",
+    houseNumber: "",
+    apartmentNumber: "",
+    city: "",
+    postalCode: "",
+  });
+  const [formVisible, setFormVisible] = useState(true);
+
+  useEffect(() => {
+    const userEmail = localStorage.getItem("email");
+
+    if (userEmail) {
+      axios
+        .get(`http://localhost:8080/api/user/address`, {
+          headers: {
+            Authorization: userEmail,
+          },
+        })
+        .then((response) => {
+          const userAddress = response.data;
+
+          if (userAddress) {
+            setNewAddress(userAddress);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching user address:", error);
+        });
+    }
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewAddress((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userEmail = localStorage.getItem("email");
+
+    if (!userEmail) {
+      console.error("User email not found in localStorage");
+      return;
+    }
+
+    axios
+      .post(`http://localhost:8080/api/user/address`, newAddress, {
+        headers: {
+          Authorization: userEmail,
+        },
+      })
+      .then((response) => {
+        console.log("Address added successfully:", response.data);
+
+        if (onAddressAdded) {
+          onAddressAdded();
+        }
+
+        setShow(true);
+        setFormVisible(false);
+      })
+      .catch((error) => {
+        console.error("Error adding address:", error);
+      });
+  };
+
+  return (
+    <div>
+      <ToastContainer
+        className="p-3"
+        position={"top-center"}
+        style={{ zIndex: 1 }}
+      >
+        <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Dodano adres</strong>
+            <small>now</small>
+          </Toast.Header>
+        </Toast>
+      </ToastContainer>
+      {formVisible && (
+        <Form onSubmit={handleSubmit}>
+          <Col>
+            <Form.Group as={Row} className="mb-3" controlId="formStreetName">
+              <Form.Label column sm="3">
+                Ulica:
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control
+                  name="streetName"
+                  value={newAddress.streetName}
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formHouseNumber">
+              <Form.Label column sm="3">
+                Numer domu:
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control
+                  name="houseNumber"
+                  value={newAddress.houseNumber}
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formApartmentNumber"
+            >
+              <Form.Label column sm="3">
+                Numer mieszkania:
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control
+                  name="apartmentNumber"
+                  value={newAddress.apartmentNumber}
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formCity">
+              <Form.Label column sm="3">
+                Miasto:
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control
+                  name="city"
+                  value={newAddress.city}
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formPostalCode">
+              <Form.Label column sm="3">
+                Kod pocztowy:
+              </Form.Label>
+              <Col sm="9">
+                <Form.Control
+                  name="postalCode"
+                  value={newAddress.postalCode}
+                  onChange={handleInputChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Zapisz zmiany
+            </Button>
+          </Col>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default AddressChange;
--- Projekt_Inżynierski/client/src/components/Account/index.jsx
+++ Projekt_Inżynierski/client/src/components/Account/index.jsx
@@ -2,62 +2,114 @@
 import React, { useState, useEffect } from "react";
 import { Container, Button } from "react-bootstrap";
 import axios from "axios";
+import styles from "./styles.module.css";
 import Navigation from "../Navigation";
 import Foot from "../Foot";
 import NameChange from "./NameChange";
+import AddressChange from "./AddressChange";
 
 const Account = () => {
-    const [userData, setUserData] = useState(null);
-    const [showForm, setShowForm] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [userAddress, setUserAddress] = useState(null); // Dodaj stan dla danych adresowych
 
-    useEffect(() => {
-        const userEmail = localStorage.getItem("email");
+  const [showNameForm, setShowNameForm] = useState(false);
+  const [showAddressForm, setShowAddressForm] = useState(false);
 
-        if (userEmail) {
-            axios
-                .get(`http://localhost:8080/api/user?email=${userEmail}`)
-                .then((response) => {
-                    setUserData(response.data);
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-        }
-    }, []);
+  useEffect(() => {
+    const userEmail = localStorage.getItem("email");
 
-    const handleButtonClick = () => {
-        setShowForm(!showForm);
-    };
+    if (userEmail) {
+      axios
+        .get(`http://localhost:8080/api/user?email=${userEmail}`)
+        .then((response) => {
+          setUserData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
 
-    const handleUserDataChange = (updatedUserData) => {
-        setUserData(updatedUserData);
-    };
+      // Pobierz dane adresowe
+      axios
+        .get(`http://localhost:8080/api/user/address`, {
+          headers: {
+            Authorization: userEmail,
+          },
+        })
+        .then((response) => {
+          setUserAddress(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user address:", error);
+        });
+    }
+  }, []);
 
-    return (
-        <div>
-            <Navigation />
-            <Container>
-                <h1>Twoje dane</h1>
-                {userData && (
-                    <div>
-                        <p>Imię: {userData.firstName}</p>
-                        <p>Nazwisko: {userData.lastName}</p>
-                        <p>Email: {userData.email}</p>
-                    </div>
-                )}
-                <Button onClick={handleButtonClick} variant="primary">
-                    Zmień imię i nazwisko
-                </Button>
-                {showForm && (
-                    <NameChange
-                        userData={userData}
-                        onUserDataChange={handleUserDataChange}
-                    />
-                )}
-            </Container>
-            <Foot />
-        </div>
-    );
+  const handleNameButtonClick = () => {
+    setShowNameForm(!showNameForm);
+  };
+
+  const handleAddressButtonClick = () => {
+    setShowAddressForm(!showAddressForm);
+  };
+
+  const handleUserDataChange = (updatedUserData) => {
+    setUserData(updatedUserData);
+  };
+
+  const handleAddressAdded = () => {
+    // Aktualizuj dane adresowe po dodaniu/adresie
+    axios
+      .get(`http://localhost:8080/api/user/address`, {
+        headers: {
+          Authorization: localStorage.getItem("email"),
+        },
+      })
+      .then((response) => {
+        setUserAddress(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user address:", error);
+      });
+  };
+
+  return (
+    <div>
+      <Navigation />
+      <Container>
+        <h1>Twoje dane</h1>
+        {userData && (
+          <div>
+            <p>Imię: {userData.firstName}</p>
+            <p>Nazwisko: {userData.lastName}</p>
+            <p>Email: {userData.email}</p>
+          </div>
+        )}
+        {userAddress && (
+          <div>
+            <p>Ulica: {userAddress.streetName}</p>
+            <p>Numer domu: {userAddress.houseNumber}</p>
+            <p>Numer mieszkania: {userAddress.apartmentNumber}</p>
+            <p>Miasto: {userAddress.city}</p>
+            <p>Kod pocztowy: {userAddress.postalCode}</p>
+          </div>
+        )}
+        <Button onClick={handleNameButtonClick} variant="primary">
+          Zmień imię i nazwisko
+        </Button>
+        {showNameForm && (
+          <NameChange
+            userData={userData}
+            onUserDataChange={handleUserDataChange}
+          />
+        )}
+        <Button onClick={handleAddressButtonClick} variant="primary">
+          Dodaj lub zmień adres
+        </Button>
+        {showAddressForm && <AddressChange onAddressAdded={handleAddressAdded} />}
+      </Container>
+      <Foot />
+    </div>
+  );
 };
 
 export default Account;
